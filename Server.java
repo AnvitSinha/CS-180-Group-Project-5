529247b1082eb47057048bf1e27c27a7e7042c23
AnvitSinha <90011065+AnvitSinha@users.noreply.github.com>
2022-04-28 11:54:05 -0400

    Updated Server (client handler).
    Client handler has switch cases to determine client's request. Server now inter-ops with MultiClientServer.java to handle multiple clients.

--- Server.java
+++ Server.java
@@ -1,81 +1,317 @@
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class Server {
-    public static void main(String[] args) {
+public class MainServer implements Runnable {
+
+    private static final Object GATEKEEPER = new Object();
+    private final Socket client;
+
+    public MainServer(Socket client) {
+
+        this.client = client;
+
+    }
+
+    public void run() {
+
         try {
-            ServerSocket serverSocket = new ServerSocket(6969);
-            Socket socket = serverSocket.accept();
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter writer = new PrintWriter(socket.getOutputStream());
 
-            ArrayList<String> usernameList = new ArrayList<String>();
-            ArrayList<String> passwordList = new ArrayList<String>();
-
+            BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
             String currentLine;
 
-            String[] usernameArray;
-            String[] passwordArray;
-
-            int verifiedNumber = 0;
-            String currentPassword = "";
-            String currentUsername = "";
-            int arrayIndex = 0;
+            Account.initializeAccounts();
+            Course.initializeCourses();
+            GradeBook.initializeStudentGradebook();
+            QuizFile q = new QuizFile("quiz_tester.txt");
 
             while (true) {
 
                 currentLine = reader.readLine();
 
-                if (currentLine.equals("Verify")) {
-                    //1: List does not contain username
-                    //2: Username and password not correct
-                    //3: Credentials valid
+                if (currentLine != null) {
 
-                    currentUsername = reader.readLine();
-                    currentPassword = reader.readLine();
+                    switch (currentLine) {
 
-                    if (!(usernameList.contains(currentUsername))) {
-                        writer.println("1");
-                    } else {
-                        for (int i = 0; i < usernameList.size(); i++) {
-                            if (usernameList.get(i).equals(currentUsername)) {
-                                arrayIndex = i;
+                        // Account options
+                        case "verify" -> {
+
+                            synchronized (GATEKEEPER) {
+
+                                String username = reader.readLine();
+                                String password = reader.readLine();
+
+                                String response = String.valueOf(Account.isValidCredential(username, password));
+
+                                writer.println(response);
+                                writer.flush();
+
                             }
+
+
                         }
-                        if (currentPassword.equals(passwordList.get(arrayIndex))) {
-                            writer.println("3");
-                        } else {
-                            writer.println("2");
+
+                        case "updateAccount" -> {
+
+                            String username = reader.readLine();
+                            String password = reader.readLine();
+                            Account account = new Account(username, password);
+
+                            int choice = Integer.parseInt(reader.readLine());   // get user's choice for what to update
+
+
+                            switch (choice) {
+
+                                case 1 -> {
+
+                                    String newUsername = reader.readLine();
+
+                                    String response = String.valueOf(account.updateUsername(newUsername));
+
+                                    writer.println(response);
+                                    writer.flush();
+
+                                }       // Update username
+
+                                case 2 -> {
+
+                                    String newPassword = reader.readLine();
+
+                                    String response = String.valueOf(account.updatePassword(newPassword));
+
+                                    writer.println(response);
+                                    writer.flush();
+
+                                }       // update password
+
+                                case 3 -> {
+
+                                    String newName = reader.readLine();
+
+                                    account.updateName(newName);
+
+                                    writer.println(true);
+                                    writer.flush();
+
+                                }       // update name
+
+                            }
+
                         }
+
+                        case "deleteAccount" -> {
+
+                            String username = reader.readLine();
+                            String password = reader.readLine();
+                            Account account = new Account(username, password);
+
+                            account.deleteAccount();
+
+                            writer.println(true);
+                            writer.flush();
+
+                        }
+
+                        case "newAccount" -> {
+
+                            String type = reader.readLine();
+                            String name = reader.readLine();
+                            String username = reader.readLine();
+                            String password = reader.readLine();
+
+                            String response = String.valueOf(Account.addNewAccount(name, username, password, type));
+
+                            writer.println(response);
+                            writer.flush();
+
+
+                        }
+
+                        // Quiz Options
+                        case "printQuiz" -> {
+                            String quizName = reader.readLine();
+                            Quiz myQuiz = q.getQuiz(quizName);
+                            writer.write(myQuiz.stringify());
+                            writer.println();
+                            writer.flush();
+                        }
+
+                        case "addQuiz" -> {
+
+                            String courseName = reader.readLine();
+                            String quizName = reader.readLine();
+
+                            writer.write(String.valueOf(q.addQuiz(courseName, quizName)));
+                            writer.println();
+                            writer.flush();
+                        }
+
+                        case "addQuestion" -> {
+                            String quizName = reader.readLine();
+                            int type = Integer.parseInt(reader.readLine());
+                            String question = reader.readLine();
+                            ArrayList<String> answers =
+                                    new ArrayList<String>(Arrays.asList(reader.readLine().split("&")));
+                            String correctAnswer = reader.readLine();
+
+                            writer.write(String.valueOf(q.addQuestion(quizName, type, question, answers, correctAnswer)));
+                            writer.println();
+                            writer.flush();
+                        }
+
+                        case "deleteQuiz" -> {
+                            String quizName = reader.readLine();
+
+                            writer.write(String.valueOf(q.deleteQuiz(quizName)));
+                            writer.println();
+                            writer.flush();
+                        }
+
+                        case "deleteQuestion" -> {
+                            String quizName = reader.readLine();
+                            int questionNum = Integer.parseInt(reader.readLine());
+
+                            writer.write(String.valueOf(q.deleteQuestion(quizName, questionNum)));
+                            writer.println();
+                            writer.flush();
+                        }
+
+                        case "editQuizName" -> {
+                            String quizName = reader.readLine();
+                            String newName = reader.readLine();
+
+                            writer.write(String.valueOf(q.editQuizName(quizName, newName)));
+                            writer.println();
+                            writer.flush();
+                        }
+
+                        case "editQuizCourse" -> {
+                            String quizName = reader.readLine();
+                            String newCourse = reader.readLine();
+
+                            writer.write(String.valueOf(q.editQuizCourse(quizName, newCourse)));
+                            writer.println();
+                            writer.flush();
+                        }
+
+                        case "editQuestion" -> {
+
+                            String quizName = reader.readLine();
+                            int questionNumber = Integer.parseInt(reader.readLine());
+                            int type = Integer.parseInt(reader.readLine());
+                            String question = reader.readLine();
+                            ArrayList<String> answers =
+                                    new ArrayList<String>(Arrays.asList(reader.readLine().split("&")));
+                            String correctAnswer = reader.readLine();
+
+                            writer.write(String.valueOf(q.editQuestion(quizName, questionNumber,
+                                    type, question, answers, correctAnswer)));
+                            writer.println();
+                            writer.flush();
+                        }
+
+                        case "printCourseQuizzes" -> {
+                            String courseName = reader.readLine();
+                            ArrayList<Quiz> myQuiz = q.getCourseQuizzes(courseName);
+
+                            StringBuilder sb = new StringBuilder();
+
+                            for (Quiz temp : myQuiz) {
+
+                                sb.append(temp.stringify());
+                            }
+
+                            writer.write(sb.toString());
+                            writer.println();
+                            writer.flush();
+                        }
+
+                        // Course options
+                        case "addCourse" -> {
+
+                            String courseName = reader.readLine();
+
+                            String response = String.valueOf(Course.addCourse(courseName));
+
+                            writer.println(response);
+                            writer.flush();
+
+                        }
+
+                        case "listCourses" -> {
+
+                            ArrayList<String> allCourses = Course.listCourses();
+
+                            writer.println(allCourses.size());
+                            writer.flush();
+
+                            for (int i = 0; i < allCourses.size(); i++) {
+
+                                writer.println(String.format("Course %d: %s",(i+1), allCourses.get(i)));
+                                writer.flush();
+
+                            }
+
+                        }
+
+                        //GradeBook Options
+                        case "standing" -> {
+
+                            double studentScore = Double.parseDouble(reader.readLine());
+
+                            String response = String.valueOf(GradeBook.calculateStanding(studentScore));
+
+                            writer.println(response);
+                            writer.flush();
+
+                        }
+
+                        case "courseAverage" -> {
+
+                            String course = reader.readLine();
+
+                            String response = String.valueOf(GradeBook.calculateCourseAverage(course));
+
+                            writer.println(response);
+                            writer.flush();
+
+                        }
+
+                        case "studentAverage" -> {
+
+                            String student = reader.readLine();
+
+                            String response = String.valueOf(GradeBook.calculateStudentAverage(student));
+
+                            writer.println(response);
+                            writer.flush();
+
+                        }
+
+                        case "studentInCourse" -> {
+
+                            String student = reader.readLine();
+                            String course = reader.readLine();
+
+                            String response = String.valueOf(GradeBook.studentCourseGrade(student, course));
+
+                            writer.println(response);
+                            writer.flush();
+
+                        }
+
                     }
 
-                }
-                if (currentLine.equals("UpdateFile")) {
-                    currentUsername = reader.readLine();
-                    currentPassword = reader.readLine();
-
-                    usernameList.add(currentUsername);
-                    passwordList.add(currentPassword);
-                }
-                if (currentLine.equals("Initialize")) {
-                    usernameArray = currentLine.split(",");
-                    currentLine = reader.readLine();
-                    passwordArray = currentLine.split(",");
-
-                    for (int i = 0; i < usernameArray.length; i++) {
-                        usernameList.add(usernameArray[i]);
-                    }
-
-                    for (int i = 0; i < passwordArray.length; i++) {
-                        passwordList.add(passwordArray[i]);
-                    }
                 }
             }
 
-        } catch (IOException e) {
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 }
